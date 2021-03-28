7c977809414f2d0ac49b0484305dac8a3fa724ff
carteak004 <carteak004@gmail.com>
2021-03-28 00:41:31 -0500

    final version of command line program

--- goHealth.js
+++ goHealth.js
@@ -1,38 +1,62 @@
-console.log('this is a rest');
-calculateBigrams('gohealth.txt');
-async function calculateBigrams(file) {
-  console.log('this is a rest');
-  var splitArray = '';
-  // var rawFile = new XMLHttpRequest();
-  // rawFile.open("GET", file, false);
-  // rawFile.onreadystatechange = function() {
-  //   if (rawFile.readyState === 4) {
-  //     if (rawFile.status === 200 || rawFile.status == 0) {
-  //       splitArray = rawFile.responseText;
-  //     }
-  //   }
-  // }
-  // rawFile.send(null);
+// Check if the file name is passed in the command line
+if(process.argv.length == 2)
+{
+  console.error('File name not found. Command expected to be node goHealth.js [file path]');
+  process.exit(1);
+}
+else
+{
+  calculateBigrams(process.argv[2]);
+}
 
-  var splitArray = await file.text();
+// Function to calculate Bigrams
+async function calculateBigrams(file)
+{
+  var fs = require('fs'); // To read the contents of the file
+  try
+  {
+    await fs.readFile(file, 'utf8', function(err, data)
+    {
+      if (err) throw err;
 
-  splitArray = splitArray.match(/[\w']+/g); //Just picked words from the text excluding new lines and punctuations. Used this for the possibility of Words can be alphanumeric
-  var bigramArray = [];
+      // Variable declaration
+      var dataFromFile = '';
+      var bigramArray = [];
 
-  for (i = 0; i < splitArray.length - 1; i++) {
-    bigramArray[i] = splitArray[i] + ' ' + splitArray[i + 1];
-    // bigramArray.push(splitArray[i] + ' ' + splitArray[i+1]);
+      dataFromFile = data.toLowerCase(); // Read the data from text file and store everything in lower case to make parsing easy
+
+      if (dataFromFile == '')
+      {
+        console.error('File empty. Aborting process');
+        process.exit(1);
+      }
+      dataFromFile = dataFromFile.match(/[\w']+/g); //Just picked words from the text excluding new lines and punctuations. Used this for the possibility of Words can be alphanumeric
+
+      // Construct bigram array
+      for (i = 0; i < dataFromFile.length - 1; i++)
+      {
+        bigramArray[i] = dataFromFile[i] + ' ' + dataFromFile[i + 1];
+      }
+
+      // Count the bigrams
+      bigramArray = bigramArray.reduce(function(obj, bigram)
+      {
+        if (!obj[bigram])
+        {
+          obj[bigram] = 1;
+        }
+        else
+        {
+          obj[bigram]++;
+        }
+        return obj;
+      }, {}); // Passed {} as initial value
+
+      console.log('Bigrams \n', bigramArray);
+    });
   }
-
-  bigramArray = bigramArray.reduce(function(obj, bigram) {
-    if (!obj[bigram]) {
-      obj[bigram] = 1;
-    } else {
-      obj[bigram]++;
-    }
-    return obj;
-  }, {});
-
-  console.log(bigramArray);
-  return bigramArray;
+  catch (e)
+  {
+    console.log('Error', e.stack);
+  }
 }
